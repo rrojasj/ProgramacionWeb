58d131f76d7c268eb30c85f07a0348d526d7bb28
Roberto Rojas <robjimn@amazon.com>
2024-02-05 03:04:15 -0600

    Tarea2 - index.html y login.html completos. Login valida el usuario solicitado.

---
+++ Tareas/Tarea2/js/index.js
@@ -0,0 +1,5 @@
+`use strict`;
+
+function continueLoginPage() {
+    window.location.href = "login.html";
+}
---
+++ Tareas/Tarea2/js/login.js
@@ -0,0 +1,27 @@
+`use strict`
+
+function userLogin () {
+    var user = document.getElementById("userID").value;
+    var pswd = document.getElementById("userPswd").value;
+    // console.log(user);
+    // console.log(pswd);
+    if (user == "cenfo" && pswd == "123"){
+        Swal.fire({
+            icon: 'success',
+            title: 'Éxito',
+            text: 'Credenciales correctos',
+          }).then((result) => {
+            // Redirige a otra página después de hacer clic en OK en el SweetAlert
+            if (result.isConfirmed) {
+              window.location.href = 'index.html';
+            }
+          });
+    }else {
+        Swal.fire({
+            icon: "error",
+            title: "Incorrecto",
+            text: "Por favor ingrese credenciales correctos."
+          });
+    };
+};
+
